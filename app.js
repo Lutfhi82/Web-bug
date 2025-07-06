f6bb80e3770b4386e3b9868a584f0bf606433380
Lutfhi82 <pohon809@gmail.com>
2025-07-06 08:42:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const DATA_URL = 'https://raw.githubusercontent.com/Lutfhi82/database-username/refs/heads/main/res.json';
+const DATA_URL = 'https://raw.githubusercontent.com/Lutfhi82/database-username/main/res.json';
 
 async function login() {
   const user = document.getElementById("username").value.trim();
